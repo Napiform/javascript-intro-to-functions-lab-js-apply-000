8700eb24bdc04be91bd30344a6597b9c293ebd23
Jeremy Cripe <jbcripe@gmail.com>
2020-01-10 23:08:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ( string.toUpperCase === string ) {
+  if ( string === string.toLowerCase() ) {
     return "I can't hear you!"
-  }
-}
+  } elsif ( string === string.toUpperCase() ) {
+    return 'YES INDEED!'
+} elsif 
